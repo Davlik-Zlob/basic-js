a4b8232df09afadf9c55314b0866dc46933255af
Davlik <slon.zelenyj@gmail.com>
2023-04-17 17:54:48 +0600

    test

--- src/what-season.js
+++ src/what-season.js
@@ -12,37 +12,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  throw new NotImplementedError('Not implemented');
-  // if (!date) {
-  //   return 'Unable to determine the time of year!';
-  // }
-  // const originalDate = new Date();
-  // if (!(date instanceof Date) || Object.prototype.toString.call(date) !== '[object Date]' ||
-  //     Number.isNaN(date.valueOf()) || typeof date.getFullYear !== 'function' ||
-  //     typeof date.getMonth !== 'function' || typeof date.getTime !== 'function' ||
-  //     typeof date.getTime() !== 'number' ||
-  //     Object.getOwnPropertyNames(date).length) {
-  //   throw new Error('Invalid date!');
-  // }
-  // const month = date.getMonth();
-  // switch (month) {
-  //   case 11:
-  //   case 0:
-  //   case 1:
-  //     return 'winter';
-  //   case 2:
-  //   case 3:
-  //   case 4:
-  //     return 'spring';
-  //   case 5:
-  //   case 6:
-  //   case 7:
-  //     return 'summer';
-  //   case 8:
-  //   case 9:
-  //   case 10:
-  //     return 'fall';
-  // }
+  // throw new NotImplementedError('Not implemented');
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  const originalDate = new Date();
+  if (!(date instanceof Date)) {
+    throw new Error('Invalid date!');
+  }
+  const month = date.getMonth();
+  switch (month) {
+    case 11:
+    case 0:
+    case 1:
+      return 'winter';
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+    case 8:
+    case 9:
+    case 10:
+      return 'fall';
+  }
   // remove line with error and write your code here
 }
 
